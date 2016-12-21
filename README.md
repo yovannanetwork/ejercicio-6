df42978d76c818af70897feb3e72c413cc1d1d20
yovannanetwork <yovannanetwork@gmail.com>
2016-12-21 12:02:15 -0600

    ejercicio terminado

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ejercicio-6
 Dormimos!
+Inicio: Dormimos
+ 
+ # ingreso de datos
+ - weekday(dia de la semana)
+ - vacations
+ - puede_dormir
+ 
+ # proceso
+ - si vacations es true
+   -mostrar 1 "dormimos"
+ - Si weekday es true
+   - mostrar 0 "dormimos"
+- si no
+ - 0
+ # fin
---
+++ main.js
@@ -0,0 +1,28 @@
+function calcular()
+{ 
+    var vacation = document.getElementById("vacaciones").value;
+    var weekday = parseInt(document.getElementById("dia").value);
+    
+    var isVacation = true;
+    var isWeekday = false;
+    
+    var salida = document.getElementById("salida");
+    
+    if(vacation == "2")
+        {isVacation = false;}
+    
+    if (weekday < 6)
+        {isWeekday = true;}
+    
+    if (isVacation)
+        {
+            salida.innerHTML = "si duerme";
+        }
+    else{
+        if(isWeekday){
+            salida.innerHTML = "no puedes dormir";
+        }else{
+            salida.innerHTML = "si duerme";
+        }
+    }
+}
